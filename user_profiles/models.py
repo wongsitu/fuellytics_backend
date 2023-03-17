56fed39c287dca6f50c5a9d63f300bad08ccc136
wongsitu <sa_waika_4d_23@hotmail.com>
2023-03-16 21:30:07 -0600

    collecstatic

--- user_profiles/models.py
+++ user_profiles/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE, related_name='profile')
     profile_image = models.ImageField(upload_to='photos', blank=True, name="profile_image")
+    first_name = models.CharField(max_length=250, blank=True)
 
     def __str__(self):
         return self.user.username
