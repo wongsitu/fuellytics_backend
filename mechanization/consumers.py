a2444ff393377778ee981636bbd1c1a76c829735
wongsitu <wongsitu@ksu.edu>
2023-03-25 19:14:43 -0600

    fix

--- mechanization/consumers.py
+++ mechanization/consumers.py
@@ -10,3 +10,12 @@ class MechanizationConsumer(WebsocketConsumer):
         for i in range(1000):
             self.send(json.dumps({'message': randint(1, 100)}))
             sleep(1)
+
+    def disconnect(self, close_code):
+        pass
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+
+        self.send(text_data=json.dumps({"message": message}))
