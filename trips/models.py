b410d22d73e1706202ef3ecd4a8336ee2e2911aa
wongsitu <sa_waika_4d_23@hotmail.com>
2023-04-02 00:22:36 -0600

    feat: added trips creation

--- trips/models.py
+++ trips/models.py
@@ -2,26 +2,22 @@ from django.db import models
 from user_profiles.models import UserProfile
 from car_profiles.models import CarProfile
 from django.contrib.postgres.fields import ArrayField
-
+from django.contrib.postgres.fields.jsonb import JSONField as JSONBField
 
 class Trip(models.Model):
     started_at = models.DateTimeField()
     ended_at = models.DateTimeField()
-    route_coordinates = ArrayField(ArrayField(models.IntegerField(), size=2))
+    route_coordinates = models.JSONField(default=list)
     user_profile = models.ForeignKey(
         UserProfile, on_delete=models.CASCADE, related_name='user_profile')
     car_profile = models.ForeignKey(
         CarProfile, on_delete=models.CASCADE, related_name='car_profile')
     fuel_consumption = models.DecimalField(
         decimal_places=3, max_digits=10, default=0.0)
-    fuel_consumption_price = models.DecimalField(
+    average_speed = models.DecimalField(
         decimal_places=3, max_digits=10, default=0.0)
     co2_emissions = models.DecimalField(
         decimal_places=3, max_digits=10, default=0.0)
-    n2o_emissions = models.DecimalField(
-        decimal_places=3, max_digits=10, default=0.0)
-    ch4_emissions = models.DecimalField(
-        decimal_places=3, max_digits=10, default=0.0)
 
     def __str__(self):
-        return self.model
+        return self.user_profile.user.username
--- trips/views.py
+++ trips/views.py
@@ -7,7 +7,10 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
-
+from trips.filters import TripFilter
+from user_profiles.models import UserProfile
+from rest_framework.response import Response
+import datetime
 
 class Trips(viewsets.ModelViewSet):
     authentication_classes = [SessionAuthentication]
@@ -15,15 +18,28 @@ class Trips(viewsets.ModelViewSet):
     serializer_class = TripSerializer
     queryset = Trip.objects.all().order_by('id')
     filter_backends = [DjangoFilterBackend]
+    filter_class = TripFilter
 
     def create(self, request):
         trip_form = TripsForm(data=request.data)
 
         if trip_form.is_valid():
             new_trip = trip_form.save(commit=False)
+            started_at = request.data.get('started_at')
+            ended_at = request.data.get('ended_at')
+            date_format = "%Y-%m-%dT%H:%M:%SZ"
+
+            formatted_started_at = datetime.datetime.strptime(started_at, date_format)
+            formatted_ended_at = datetime.datetime.strptime(ended_at, date_format)
+
+            new_trip.started_at = formatted_started_at
+            new_trip.ended_at = formatted_ended_at
+
+            user_profile, created = UserProfile.objects.get_or_create(user=request.user)
+            new_trip.user_profile = user_profile
+
             car_profile_id = request.data.get('car_profile_id')
             car_profile = get_object_or_404(CarProfile, id=car_profile_id)
-            new_trip.user = request.user
             new_trip.car_profile = car_profile
             new_trip.save()
 
