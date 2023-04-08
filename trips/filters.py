9f3cba6bd8b8dbc07f66b3bd144a72746ebd078e
wongsitu <sa_waika_4d_23@hotmail.com>
2023-04-08 12:32:49 -0600

    date range filters

--- trips/filters.py
+++ trips/filters.py
@@ -4,7 +4,7 @@ from django_filters import rest_framework
 class TripFilter(rest_framework.FilterSet):
     user_id = rest_framework.CharFilter(
         field_name='user_profile__user__id', lookup_expr='iexact')
-    started_at = rest_framework.CharFilter(
-        field_name='started_at', lookup_expr='icontains')
-    ended_at = rest_framework.CharFilter(
-        field_name='ended_at', lookup_expr='icontains')
+    started_at = rest_framework.DateFilter(
+        field_name='started_at', lookup_expr='gte')
+    ended_at = rest_framework.DateFilter(
+        field_name='ended_at', lookup_expr='lte')
